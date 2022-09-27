0aab7631b48de0366ea7e64afc7650949d30b3be
cunfish <114482729+cunfish@users.noreply.github.com>
2022-09-27 18:55:26 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+// JavaScript source code
+
+
+window.onload = function () {
+    var hit = document.getElementsByClassName("letter")[0]
+    hit.onclick = function () {
+        alert("还没想好说什么");
+    }
+
+	
+
+}
